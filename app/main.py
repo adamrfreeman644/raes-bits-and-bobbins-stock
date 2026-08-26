0753307bc44c66462d4934297a0660772b74cb72
adamrfreeman644 <adamfreeman644@gmail.com>
2026-08-27 00:24:18 +0100

    Fix duplicate Photo Shoot blueprint registration and version override

--- app/main.py
+++ app/main.py
@@ -1,8 +1,1 @@
-from app import server
-from app.photoshoot import bp as photoshoot_bp, init_tables
-
-server.VERSION = '0.0.6'
-server.app.register_blueprint(photoshoot_bp)
-init_tables()
-
-app = server.app
+from app.server import app
